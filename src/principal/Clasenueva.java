fa3d005e0b1e694a1e8294421af874c414ddd909
MdelosReyes <mariadelosreyesgn@gmail.com>
2021-03-19 18:04:32 +0100

    Creación de la clase Clasenueva

--- src/principal/Clasenueva.java
+++ src/principal/Clasenueva.java
@@ -3,7 +3,8 @@ package principal;
 public class Clasenueva {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("En la rama. Se crea la clase nueva");
 
 	}
 
