b60e4b4256901e947f635b0d5f7460c255cd27c4
MdelosReyes <mariadelosreyesgn@gmail.com>
2021-03-19 17:32:02 +0100

    Líneas en método main

---
+++ src/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Probando Eclipse con Git y GitHub.com");
+		System.out.println("Cogiendo práctica");
+	}
+
+}
